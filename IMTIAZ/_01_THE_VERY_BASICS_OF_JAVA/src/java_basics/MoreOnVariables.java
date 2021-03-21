13c88e86c2b204ad93ca591bca6899e2484fd616
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-03-21 16:53:00 -0300

    20210321

---
+++ IMTIAZ/_01_THE_VERY_BASICS_OF_JAVA/src/java_basics/MoreOnVariables.java
@@ -0,0 +1,24 @@
+package java_basics;
+
+public class MoreOnVariables {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		
+		long big_number = 10000000000L;
+		float fragment = 1.002F;
+		double fraction = 1.002;
+		
+		/* Boolean */
+		boolean verity = true;
+		var faux = false;
+		
+		
+	}
+
+}
